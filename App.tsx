aeb6ae666954d4f78e0d254cbe8a9181dd3fe9ed
Heitor <heitor.vrb@gmail.com>
2022-12-23 06:41:31 -0300

    Generate lucky numbers on page load

--- App.tsx
+++ App.tsx
@@ -2,9 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const numbers = [
+    Math.floor(Math.random() * 60) + 1,
+    Math.floor(Math.random() * 60) + 1,
+    Math.floor(Math.random() * 60) + 1,
+    Math.floor(Math.random() * 60) + 1,
+    Math.floor(Math.random() * 60) + 1,
+    Math.floor(Math.random() * 60) + 1,
+  ];
+  numbers.sort();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Seus números da sorte são:</Text>
+      <Text>{numbers.join(' - ')}</Text>
       <StatusBar style="auto" />
     </View>
   );
